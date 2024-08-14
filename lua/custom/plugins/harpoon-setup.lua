22391f8b3ab3cc572bb0928011851aeb4fee2aa2
Luigi-Tho <33259668+Luigi-Tho@users.noreply.github.com>
2024-08-14 08:48:12 -0400

    changed harpoon bindings

--- lua/custom/plugins/harpoon-setup.lua
+++ lua/custom/plugins/harpoon-setup.lua
@@ -3,9 +3,9 @@ return {
 	config = function ()
 		
 		require("harpoon").setup({
-		  vim.keymap.set("n", "<leader>hp", function () require("harpoon.ui").toggle_quick_menu() end, {desc = "[h]ar[p]oon quick menu"}),
-		  vim.keymap.set("n", "<leader>hpm", function () require("harpoon.mark").add_file() end, {desc = "[h]ar[p]oon [M]ark file"}),
-		  vim.keymap.set("n", "<leader>t", function () require("harpoon.term").gotoTerminal(1) end, {desc = "go to [t]erminal"}),
+		  vim.keymap.set("n", "<leader>h", function () require("harpoon.ui").toggle_quick_menu() end, {desc = "[h]ar[p]oon quick menu"}),
+		  vim.keymap.set("n", "<leader>m", function () require("harpoon.mark").add_file() end, {desc = "[h]ar[p]oon [M]ark file"}),
+		  -- vim.keymap.set("n", "<leader>t", function () require("harpoon.term").gotoTerminal(1) end, {desc = "go to [t]erminal"}),
 		})
 	end
 }
